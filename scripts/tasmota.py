d753f5938f6e2347668566a1a6ea61fa55baf3a7
Nama <mrt690@gmail.com>
2021-04-10 12:18:23 +0200

    Handle ReadTimeout exception on requests

--- scripts/tasmota.py
+++ scripts/tasmota.py
@@ -5,7 +5,7 @@ from json import loads
 from time import sleep
 from threading import Thread
 from requests import get
-from requests.exceptions import ConnectionError, ChunkedEncodingError
+from requests.exceptions import ConnectionError, ChunkedEncodingError, ReadTimeout
 from artemisremotecontrol.config import Config
 from artemisremotecontrol import getleds, setleds
 
@@ -26,7 +26,7 @@ def loop():
                 try:
                     response = get(url, timeout=1)
                     status = response.status_code
-                except (ConnectionError, ChunkedEncodingError):
+                except (ConnectionError, ChunkedEncodingError, ReadTimeout):
                     status = False
                 if status != 200:
                     # send "Disconnected" state
